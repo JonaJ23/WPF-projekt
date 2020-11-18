6591ed5d6ad74a67f636f8b44db8ac49366a0615
Jonatan Jonsson <jonatan.jonsson@utb.ecutbildning.se>
2020-11-18 12:11:30 +0100

    La till "UserInfo" för att skapa en kundsida, där man kan se sin info samt vilka filmer man hyrt.

---
+++ Store/UserInfo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Store
+{
+    /// <summary>
+    /// Interaction logic for UserInfo.xaml
+    /// </summary>
+    public partial class UserInfo : Window
+    {
+        public UserInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
