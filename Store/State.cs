ca978357be8517542a2bef615cf0a2d7bac2c2f0
Christopher Bergman <christopher.bergman@learnet.se>
2020-11-18 10:04:46 +0100

    La till en CustomerInfo som vi man använda (Test för se om github fungerar)

--- Store/State.cs
+++ Store/State.cs
@@ -12,5 +12,7 @@ namespace Store
         public static Customer User { get; set; }
         public static List<Movie> Movies { get; set; }
         public static Movie Pick { get; set; }
+
+        public static CustomerInfo customerInfo { get; set; }
     }
 }
