903b354a4c437f02bd8670a5d0456be8706d5a9d
Jonatan Jonsson <jonatan.jonsson@utb.ecutbildning.se>
2020-11-21 14:08:27 +0100

    La till pop-fönster som uppmärksammar tom eller fel inmatning vid "LoginWindow".

--- Store/LoginWindow.xaml.cs
+++ Store/LoginWindow.xaml.cs
@@ -30,20 +30,18 @@ namespace Store
             State.User = API.GetCustomerByName(NameField.Text.Trim());
             if (State.User != null)
             {
-                if(State.User.Password == PasswordField.Password)
-                { 
-                var next_window = new MainWindow();
-                next_window.Show();
-                this.Close();
-                }
-                else
+                if (State.User.Password == PasswordField.Password)
                 {
-                    MessageBox.Show("Wrong password, try again.");
+                    var next_window = new MainWindow();
+                    next_window.Show();
+                    this.Close();
                 }
             }
             else
             {
                 NameField.Text = "...";
+                NameField.Text = ".....";
+                MessageBox.Show("Wrong username/password, try again.", "Wrong input", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
